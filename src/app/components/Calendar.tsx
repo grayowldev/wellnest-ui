d25620e0ee9cb0ede32b1e6108b63d377a727b4a
kwasinsiah <truegrayowl@gmail.com>
2024-09-20 00:55:21 -0400

    Added calender

---
+++ src/app/components/Calendar.tsx
@@ -0,0 +1,91 @@
+'use client'
+import dayjs from 'dayjs';
+import React, { useState } from 'react';
+import {cn} from "@/app/components/cn";
+import {log} from "util";
+
+
+export const generateDate = (
+    month = dayjs().month(), 
+    year = dayjs().year()
+) => {
+    const firstDayOfMonth = dayjs().year(year).month(month).startOf("month")
+    const lastDayOfMonth = dayjs().year(year).month(month).endOf("month")
+    const dates = []
+
+    let i = 0
+    
+    while (i < firstDayOfMonth.day()) {
+        dates.push(
+            {
+                date: firstDayOfMonth.day(i),
+                isCurrentMonth: false,
+            })
+        i++;
+    }
+
+    // dates for the month
+    i = 1
+    while (i <= lastDayOfMonth.date()) {
+        dates.push({
+            date: firstDayOfMonth.date(i),
+            isCurrentMonth: true,
+            today: firstDayOfMonth.date(i).toDate().toDateString() === dayjs().toDate().toDateString(),
+    })
+        i++;
+    }
+
+    const remainder = 35 - dates.length
+
+    i = lastDayOfMonth.date() + 1
+    while (i <= lastDayOfMonth.date() + remainder) {
+        dates.push({
+            date: lastDayOfMonth.date(i),
+            isCurrentMonth: false
+    })
+        i++;
+    }
+
+    
+
+
+    return dates;
+    
+
+}
+
+interface CalendarProps {
+  onSelectDate: (date: Date) => void;
+}
+
+const Calendar = () => {
+
+  console.log( generateDate() );
+   const days = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+
+
+  return (
+    <div className='w-96 h-96 grid grid-cols-7 text-sm'>
+        {days.map((day, index) => {
+            return (
+                <div key={index}>
+                    <h1 className={`h-14 grid place-content-center`}>{day}</h1>
+                </div>
+            )
+        })}
+        {generateDate().map(({date, isCurrentMonth, today}, index) => {
+            return (
+                <div key={index} className={`h-14 border-t grid place-content-center`}>
+                    <h1 className={cn(
+                        isCurrentMonth ? "": "text-gray-400",
+                        today ? "bg-blue-600 text-white" : "",
+                        "h-10 w-10 rounded-full grid place-content-center hover:bg-black hover:text-white transition-all cursor-pointer"
+                    )}>{date.date()}</h1>
+                </div>
+            )
+        })}
+    </div>
+  );
+};
+
+export default Calendar;
