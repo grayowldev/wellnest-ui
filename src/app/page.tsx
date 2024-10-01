e0b9b718a26395ed6ff3dc689056376097b0c79f
kwasinsiah <truegrayowl@gmail.com>
2024-09-30 21:59:43 -0400

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,11 @@
-'use client'
-import { useState } from 'react';
+import Home from "@/app/views/Home";
+import {redirect} from "next/navigation";
 
-import AppointmentList from './components/AppointmentList';
-import AppointmentForm  from './components/AppointmentForm';
-import Calendar, { generateDate } from './components/Calendar';
-import {Signup} from "@/app/views/Signup";
+export default function App(){
+    const isLoggedIn = true;
 
-export default function Home() {
-  const [appointments, setAppointments] = useState([]);
-  // const [selectedDate, setSelectedDate] = useState(null);
-
-  const addAppointment = (newAppointment) => {
-    setAppointments([...appointments, { ...newAppointment, id: Date.now() }]);
-  };
-
-
-  return (
-    <main className="container mx-auto p-4">
-        <Signup />
-      <h1 className="text-3xl font-bold mb-4">Appointment Scheduler</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <AppointmentForm onAddAppointment={addAppointment} />
-        <AppointmentList appointments={appointments} />
-        <Calendar/>
-      </div>
-    </main>
-  );
+    if (!isLoggedIn) {
+            redirect( '/signin')
+    }
+    return <Home />
 }
