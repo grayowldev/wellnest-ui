38ef6c62a3b1d02f304a15c3ffe21d55dc846daf
kwasinsiah <truegrayowl@gmail.com>
2024-10-01 23:54:01 -0400

    .

--- src/app/components/AppointmentList.tsx
+++ src/app/components/AppointmentList.tsx
@@ -1,17 +1,21 @@
+import {AppointmentCard} from "@/app/components/AppointmentCard";
+
 export default function AppointmentList({ appointments }) {
   return (
-    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div className="bg-white rounded px-8 pt-6 pb-8 mb-4">
       <h2 className="text-2xl mb-4">Upcoming Appointments</h2>
       {appointments.length === 0 ? (
         <p>No appointments scheduled.</p>
       ) : (
-        <ul>
-          {appointments.map((appointment) => (
-            <li key={appointment.id} className="mb-2">
-              {appointment.date} at {appointment.time} - {appointment.name}
-            </li>
-          ))}
-        </ul>
+          <div>
+              {appointments.map((appointment, index) => (
+                  <div key={index} className={`pb-3`}>
+                      <AppointmentCard  meetingInfo={appointment} />
+                  </div>
+
+              ))}
+          </div>
+
       )}
     </div>
   );
