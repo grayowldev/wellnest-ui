91d6a729715a4ce26974a790622e196e0c3c284e
kwasinsiah <truegrayowl@gmail.com>
2024-10-14 12:04:38 -0400

    Updated calender to view next and previous months.

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -1,45 +1,88 @@
-// 'use client'
+'use client'
 import dayjs from 'dayjs';
-import React from 'react';
+import React, {useState} from 'react';
 import {cn} from "@/app/components/cn";
+import {ChevronLeft, ChevronRight} from "lucide-react";
 
 
 
 
 const Calendar = ({availableDates, setSelectedDate}) => {
+    const [month, setMonth] = useState(dayjs().month());
+    const [year, setYear] = useState(dayjs().year());
+
    const days = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+   const debugMode = true
+
+
+    // TODO: Error when year changes to the next or previous year
+
+   const gotoNextMonth = () => {
+
+       if (debugMode) {
+           console.log("button was clicked")
+           console.log(month)
+           console.log(year)
+       }
+       setMonth(month + 1)
+       setYear(dayjs().year() + Math.floor((month + 1) / 12))
+   }
+
+   const gotoPreviousMonth = () => {
+       if (debugMode) {
+           console.log("button was clicked")
+           console.log(month)
+           console.log(year)
+       }
+       setMonth(month - 1)
+       setYear(dayjs().year() - Math.floor((month - 1) / 12))
+   }
 
   return (
-    <div className='w-96 h-96 grid grid-cols-7 text-sm'>
-        {days.map((day, index) => {
-            return (
-                <div key={index}>
-                    <h1 className={`h-14 grid place-content-center`}>{day}</h1>
-                </div>
-            )
-        })}
-        {generateDate().map(({date, isCurrentMonth, today}, index) => {
-            // console.log("date:", date.now())
-            const isAvailable = availableDates?.find(d => d.year === date.year() &&
-                d.month === date.month() &&
-                d.days === date.date())
-            return (
-                <div
-                    key={index}
-                    className={`h-14 border-t grid place-content-center`}
-                    onClick={() => {
-                        isAvailable && setSelectedDate(date)
-                    }}>
-                    <h1 className={cn(
-                        isCurrentMonth ? "": "text-gray-400",
-                        today ? "bg-blue-600 text-white" : "",
-                        isAvailable ? "bg-gray-200" : "",
-                        "h-10 w-10 rounded-full grid place-content-center hover:bg-black hover:text-white transition-all cursor-pointer"
-                    )}>{date.date()}</h1>
-                </div>
-            )
-        })}
-    </div>
+      <div className={`w-fit rounded-2xl p-4 border-2`}>
+          <div className={`w-96 grid grid-cols-[62px,1fr,62px]`}>
+              <div className={`flex justify-center cursor-pointer`}
+                   onClick={gotoPreviousMonth}>
+                  <ChevronLeft></ChevronLeft>
+              </div>
+              <div className={`flex justify-center`}> {dayjs().year(year).month(month).format('MMMM YYYY')}</div>
+              <div className={`flex justify-center cursor-pointer`}
+                   onClick={gotoNextMonth}>
+                  <ChevronRight></ChevronRight>
+              </div>
+          </div>
+          <div className='w-96 h-96 grid grid-cols-7 text-sm'>
+              {days.map((day, index) => {
+                  return (
+                      <div key={index}>
+                          <h1 className={`h-14 grid place-content-center`}>{day}</h1>
+                      </div>
+                  )
+              })}
+              {generateDate(month, year).map(({date, isCurrentMonth, today}, index) => {
+                  // console.log("date:", date.now())
+                  const isAvailable = availableDates?.find(d => d.year === date.year() &&
+                      d.month === date.month() &&
+                      d.days === date.date())
+                  return (
+                      <div
+                          key={index}
+                          className={`h-14 border-t grid place-content-center`}
+                          onClick={() => {
+                              isAvailable && setSelectedDate(date)
+                          }}>
+                          <h1 className={cn(
+                              isCurrentMonth ? "": "text-gray-400",
+                              today ? "bg-blue-600 text-white" : "",
+                              isAvailable ? "bg-gray-200" : "",
+                              "h-10 w-10 rounded-full grid place-content-center hover:bg-black hover:text-white transition-all cursor-pointer"
+                          )}>{date.date()}</h1>
+                      </div>
+                  )
+              })}
+          </div>
+      </div>
+
   );
 };
 
@@ -84,12 +127,7 @@ export const generateDate = (
         i++;
     }
 
-
-
-
     return dates;
-
-
 }
 
 export default Calendar;
